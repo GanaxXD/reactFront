052d27c7b89806423ee9fdc02a0a3fb47d1ab313
GanaxXD <pedro.mattis.df@gmail.com>
2021-04-15 00:52:02 -0300

    Comite inicial da estrutura básica do projeto

---
+++ reactclient/src/components/Form.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react';
+import ReactBootstrap, {Container, Row, Col} from 'react-bootstrap';
+
+const Form = (props) => {
+
+    return (
+        <Fragment>
+            <Container fluid>
+                <Row>
+                    <Col>1 of 1</Col>
+                </Row>
+            
+            </Container>
+        </Fragment>
+    );
+}
+
+export default Form;
