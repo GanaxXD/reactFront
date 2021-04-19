d511c32026454afd4e6ce4f07942611819ed3cba
GanaxXD <pedro.mattis.df@gmail.com>
2021-04-18 22:42:56 -0300

    Criação das telas de cadastro da Ordem de serviço e dos comentários

--- reactclient/src/components/NavBarApp.js
+++ reactclient/src/components/NavBarApp.js
@@ -5,17 +5,17 @@ const NavBarApp = ()=>{
     return(
         <Fragment>
             <div margin-top = "5px">
-                <Navbar bg="dark" expand="lg" variant="dark" className="mr-auto">
-                    <Navbar.Brand href="/" font-color="white">CADASTRO DE CLIENTES E ORDENS DE SERVIÇO</Navbar.Brand>
+                <Navbar expand="lg" bg="dark" variant="dark" className="mr-auto">
+                    <Navbar.Brand href="/">CADASTRO DE CLIENTES E ORDENS DE SERVIÇO</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/clientes">Cadastrar Cliente</Nav.Link>
-                        <Nav.Link href="/ordemservico">Cadastrar Ordem de Serviço</Nav.Link>
-                        <Nav.Link href="/comentarios">Cadastrar Comentário</Nav.Link>
-                        <Nav.Link href="/listaclientes">Listar Clientes</Nav.Link>
-                        <Nav.Link href="listaordens">Listar Ordens de Serviço</Nav.Link>
-                    </Nav>
+                        <Nav className="mr-auto" >
+                            <Nav.Link href="/clientes">Cadastrar Cliente</Nav.Link>
+                            <Nav.Link href="/ordemservico">Cadastrar Ordem de Serviço</Nav.Link>
+                            <Nav.Link href="/comentarios">Cadastrar Comentário</Nav.Link>
+                            <Nav.Link href="/listaclientes">Listar Clientes</Nav.Link>
+                            <Nav.Link href="listaordens">Listar Ordens de Serviço</Nav.Link>
+                        </Nav>
                     </Navbar.Collapse>
                 </Navbar>
             </div>
